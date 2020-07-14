56aa637a5e0715b15318381e8a10c9ff830a14f5
Emeka Nkurumeh <emekankurumeh@outlook.com>
2020-07-14 00:17:56 -0500

    temporarily commented stuff up

---
+++ src/Bindings/OpenCL.hs
@@ -0,0 +1,10 @@
+module Bindings.OpenCL
+  ( module Bindings.OpenCL.Constants,
+    module Bindings.OpenCL.Functions,
+    module Bindings.OpenCL.Types,
+  )
+where
+
+import Bindings.OpenCL.Constants
+import Bindings.OpenCL.Functions
+import Bindings.OpenCL.Types
